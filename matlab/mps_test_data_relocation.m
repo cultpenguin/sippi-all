b99e43e26f4140db55ffeb759c5fa0f4682ba3af
Thomas Mejer Hansen <thomas.mejer.hansen@gmail.com>
2016-04-01 14:34:15 +0200

    matlab

--- matlab/mps_test_data_relocation.m
+++ matlab/mps_test_data_relocation.m
@@ -17,13 +17,16 @@ for img=0:1:mg;
     figure(1);set_paper;
     subplot(mg+1,3,1+(img)*3);imagesc(O.x,O.y,D1);axis image;caxis([-1 1])
     ylabel(sprintf('NMG=%d',img))
-    hold on
-    scatter(d_hard(:,1),d_hard(:,2),18,d_hard(:,4),'o');
-    hold off
+    hold on;plot(d_hard(:,1),d_hard(:,2),'ko');hold off
+    %hold on
+    %scatter(d_hard(:,1),d_hard(:,2),18,d_hard(:,4),'o');
+    %hold off
     title('Before simulation')
     subplot(mg+1,3,2+(img)*3);imagesc(O.x,O.y,D2);axis image;caxis([-1 1])
+    hold on;plot(d_hard(:,1),d_hard(:,2),'ko');hold off
     title('After simulation')
     subplot(mg+1,3,3+(img)*3);imagesc(O.x,O.y,D3);axis image;caxis([-1 1])
+    hold on;plot(d_hard(:,1),d_hard(:,2),'ko');hold off
     title('After relocations')
     %if img==0;
     %    subplot(1,3,1);title('Before simulation')
