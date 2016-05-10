9d9fa6012a1ae0f40ade5295ca67a4ca0bf24129
Thomas Mejer Hansen <thomas.mejer.hansen@gmail.com>
2016-05-10 10:50:25 +0000

    Updating license

--- mpslib/Coords4D.cpp
+++ mpslib/Coords4D.cpp
@@ -1,17 +1,19 @@
-// (c) 2016 I-GIS (www.i-gis.dk) and Solid Earth Geophysics, Niels Bohr Institute (http://imgp.nbi.ku.dk)
+// (c) 2015-2016 I-GIS (www.i-gis.dk) and Solid Earth Geophysics, Niels Bohr Institute (http://imgp.nbi.ku.dk)
 //
-//    This program is free software: you can redistribute it and/or modify
+//    This file is part of MPSlib.
+//
+//    MPSlib is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU Lesser General Public License as published by
 //    the Free Software Foundation, either version 3 of the License, or
 //    (at your option) any later version.
 //
-//    This program is distributed in the hope that it will be useful,
+//    MPSlib is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //    GNU Lesser General Public License for more details.
 //
 //    You should have received a copy of the GNU Lesser General Public License
-//    along with this program (COPYING.LESSER).  If not, see <http://www.gnu.org/licenses/>.
+//    along with MPSlib (COPYING.LESSER).  If not, see <http://www.gnu.org/licenses/>.
 //
 #include <iostream>
 #include "Coords4D.h"
