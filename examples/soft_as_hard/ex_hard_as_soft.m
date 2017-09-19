7f2b9fdfbe72524b8fb845b2c751e4f1f4fb6727
Thomas Mejer Hansen <tmeha@nbi.ku.dk>
2017-09-19 23:11:55 +0200

    HardSoftEx

--- examples/soft_as_hard/ex_hard_as_soft.m
+++ examples/soft_as_hard/ex_hard_as_soft.m
@@ -1,35 +1,45 @@
-clear all;close all;
-O=mps_snesim_read_par('snesim_soft_as_hard.par');
-SIM=ones(O.simulation_grid_size(2),O.simulation_grid_size(1)).*NaN;
+clear all;
+SIM=ones(30,30).*NaN;
 TI=read_eas_matrix('ti.dat');%
 cmap=cmap_linear([1 1 1; 1 0 0; 0 0 0]);
 
+O.parameter_filename='mps_genesim_test.txt';
+O.method='mps_genesim';
 O.hard_data_filename='dummy';
 O.soft_data_filename='dummy';
+O.soft_data_filename='soft_as_softhard.dat';
+O.n_max_ite=100000;
+O.n_max_cpdf_count=10;
+O.max_search_radius=[10000 10000];
 
-%O.hard_data_filename='hard_as_hard.dat';
-O.soft_data_filename='soft_as_hard.dat';
+O.debug=1;
+O.n_real=40;
+O.clean = 1;
+%O.exe_root = 'E:\Users\tmh\RESEARCH\PROGRAMMING\GITHUB\MPSLIB\msvc2017\x64\Release';
 
+O.n_cond = [25 1];O.shuffle_simulation_grid=2;
+[r{1},Oo{1}]=mps_cpp_thread(TI,SIM,O);
+O.n_cond = [25 3];O.shuffle_simulation_grid=2;
+[r{2},Oo{2}]=mps_cpp_thread(TI,SIM,O);
+O.n_cond = [25 1];O.shuffle_simulation_grid=1;
+[r{3},Oo{3}]=mps_cpp_thread(TI,SIM,O);
+O.n_cond = [25 3];O.shuffle_simulation_grid=1;
+[r{4},Oo{4}]=mps_cpp_thread(TI,SIM,O);
+
+%%
+figure(1);clf;
+for i=1:length(Oo)
+    subplot(2,2,i);
+    imagesc(etype(r{i}));
+    axis image;
+    caxis([0 1]);
+    title(sprintf('t=%3.1f p%d,ncs=%d ',Oo{i}.time,Oo{i}.shuffle_simulation_grid,Oo{i}.n_cond(2)))
+end
+print_mul(sprintf('%s_nMaxCpdf%03d','ex_hard_as_soft',O.n_max_cpdf_count))
 
 
 %%
-O.filename_parameter='mps_snesim.txt';
-nmg=3;
-O.n_multiple_grids=nmg;
-O.debug=3;
-O.n_real=1;
-O.template_size=[10 10 1];
-O.template_size=[7 7 1];
-O.clean = 1;
-
-%O.exe_root = 'E:\Users\tmh\RESEARCH\PROGRAMMING\GITHUB\MPSLIB\msvc2017\x64\Release';
-
-O.shuffle_simulation_grid=2;
-O.entropyfactor_simulation_grid=100000;
-
-[r,Oo]=mps_cpp(TI,SIM,O);
-
-mps_cpp_plot(r,Oo,1);
+%mps_cpp_plot(r,Oo,1);
 
 
 
