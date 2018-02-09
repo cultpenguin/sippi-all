e945e9ea15efbb2086a6ad425bba5d4eac2c634f
Thomas Mejer Hansen <tmeha@nbi.ku.dk>
2018-02-09 08:28:34 +0100

    Template

--- python/examples/mpslib_snesim_varying_template.py
+++ python/examples/mpslib_snesim_varying_template.py
@@ -9,16 +9,17 @@ import matplotlib.gridspec as gridspec
 
 O1=mps.mpslib(method='mps_snesim_tree')
 
-TI1, TI_filename1 = mps.trainingimages.strebelle(2, coarse3d=1)
+#TI1, TI_filename1 = mps.trainingimages.strebelle(2, coarse3d=1)
+TI1, TI_filename1 = mps.trainingimages.strebelle()
 O1.ti=TI1
 
-O1.par['n_multiple_grids']=23;
-O1.par['n_cond']=25
-O1.par['n_real']=1
+O1.par['n_multiple_grids']=3;
+O1.par['n_cond']=49
+O1.par['n_real']=3
 O1.par['rseed']=1
 O1.par['debug_level']=-1
-O1.par['simulation_grid_size'][0]=135
-O1.par['simulation_grid_size'][1]=100
+O1.par['simulation_grid_size'][0]=2*135
+O1.par['simulation_grid_size'][1]=2*100
 
 r1 = 8 # template size in the coarse grid
 r2 = [8,7,6,5,4,3,2,1] # template size in the finest grid
@@ -41,8 +42,9 @@ for ir in range(len(r2)):
     t.append(t1 - t0)
 
 # Plot the realizations and a bar of the timing
-plt.figure(1)
+#plt.figure(1)
 fig = plt.figure(figsize=(15, 15))
+#fig = plt.figure(1)
 outer = gridspec.GridSpec(3, 3, wspace=0.2, hspace=0.2)
 
 for ir in range(len(r2)):
