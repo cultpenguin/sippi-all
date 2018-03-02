ad57c0bdfa6e338a2e3ec431e9073c1bfe712c28
Thomas Mejer Hansen <Thomas.Mejer.Hansen@gmail.com>
2018-03-02 12:27:01 +0000

    RGB Matrix example, precomputing MPS realizations to avoid write to disc error

--- scikit-mps/examples/mpslib_example_rgbmatrix.py
+++ scikit-mps/examples/mpslib_example_rgbmatrix.py
@@ -1,51 +1,55 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# Visualizing simulstions on RGB MATRIX,
 
-# mpslib_example.py
-# example of using mpslib.py
 
-import mpslib as mps;
-import matplotlib.pyplot as plt;
-#plt.ion();
-
+# import 
 import sys
 import numpy as np
-from time import sleep
-from random import randint
-#from sense_emu import SenseHat
-#sense = SenseHat()
-
 import time
+from random import randint
+
+# import mpslib
+import mpslib as mps;
+
+# import the RGBMatrix library from
+# https://github.com/hzeller/rpi-rgb-led-matrix/tree/master/bindings
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from PIL import Image
 
-#%% MPS_SNESIM_TREE
-O1 = mps.mpslib(method='mps_snesim_tree',hard_data_fnam = 'mps_2d_hard_data.dat',
-                    n_real = 1, verbose_level=1)
+
+#%% Setup MPSlib
+O1 = mps.mpslib(method='mps_genesim', n_real = 1, verbose_level=-1)
 #O1.par['simulation_grid_size'][0]=matrix.width;
 #O1.par['simulation_grid_size'][1]=matrix.height;
 O1.par['simulation_grid_size'][0]=32;
 O1.par['simulation_grid_size'][1]=32;
-O1.parameter_filename = 'mps_snesim.txt'
 O1.par['debug_level']=-1;
-O1.par['ti_fnam'] = 'ti.dat';
 O1.par['n_cond']=9;
-O1.run()
-print(O1.sim[0])
+#O1.ti= mps.trainingimages.maze()[0]
+O1.ti= mps.trainingimages.checkerboard()[0]
+O1.ti = O1.ti[10:300:2][:,10:300:2]
+O1.par['rseed']=0;
 
-
-
-# Configuration for the matrix
+#%% Configuration for the RGB matrix
 options = RGBMatrixOptions()
 options.rows = 32
 options.chain_length = 1
 options.parallel = 1
 options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
+
+O1.run()
+print(O1.sim[0])
+np.save('file1',O1.sim[0])
+
 matrix = RGBMatrix(options = options)
+np.save('file2',O1.sim[0])
+
+
 
 RGB = np.zeros((matrix.width,matrix.height,3))
 for ix in range(32):
-            for iy in range(32):
-                            RGB[iy,ix,1]=100*O1.sim[0][iy,ix]
+    for iy in range(32):
+        RGB[iy,ix,1]=100*O1.sim[0][iy,ix]
 
 
 image = Image.fromarray(np.uint8(RGB))
@@ -86,7 +90,7 @@ while i<1:
 			g = v*g_rand  	
 			b = v*b_rand  	
 			#sense.set_pixel(x, y, r, g, b)
-			#sleep(.01)
+			#time.sleep(.01)
 	print(O1.sim[0])#sleep(1)
 
 #%%
