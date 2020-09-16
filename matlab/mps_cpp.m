06855084e8326b5b370c34207cbb482ac7f8942a
Thomas Mejer Hansen <thomas.mejer.hansen@gmail.com>
2020-09-16 11:20:45 +0200

    Local

--- matlab/mps_cpp.m
+++ matlab/mps_cpp.m
@@ -291,22 +291,26 @@ if isfield(O,'doEstimation');
         if isfield(O,'d_hard')&&(O.simulation_grid_size(3)==1);
             s=size(O.cg);nc=s(end);
             vals=[0:1:nc-1];
+            try
             for i=1:size(O.d_hard,1);
                 P=zeros(1,nc);
                 P(O.d_hard(i,4)==vals)=1;
-                if O.grid_cell_size(1)~=1
-                    ix1=ceil((O.d_hard(i,1)-O.origin(1))/O.grid_cell_size(1));
-                else
-                    ix1=O.d_hard(i,1);
-                end
-                if O.grid_cell_size(2)~=1
-                    iy1=ceil((O.d_hard(i,2)-O.origin(2))/O.grid_cell_size(2));
-                else
-                    iy1=O.d_hard(i,1);
-                end
+                %if O.grid_cell_size(1)~=1
+                    ix1=1+ceil((O.d_hard(i,1)-O.origin(1))/O.grid_cell_size(1));
+                %else
+                %    ix1=O.d_hard(i,1);
+                %end
+                %if O.grid_cell_size(2)~=1
+                    iy1=1+ceil((O.d_hard(i,2)-O.origin(2))/O.grid_cell_size(2));
+                %else
+                %    iy1=O.d_hard(i,1);
+                %end
                 for ic=1:nc
                     O.cg(iy1,ix1,ic)=P(ic);
                 end
+            end
+            catch
+                disp('O.cg prob')
             end
         end
         
