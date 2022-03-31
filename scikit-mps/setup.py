5448dc9ca4f847ddbb7f25adfafd868c0f5088b9
thomas mejer hansen <thomas.mejer.hansen@gmail.com>
2022-03-31 13:31:02 +0200

    PyPI

--- scikit-mps/setup.py
+++ scikit-mps/setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name = "scikit-mps",
-    version = "0.3.0",
+    version = "0.3.1",
     description = 'Multiple point statistical (MPS) simulation',
     long_description=long_description,
     long_description_content_type='text/x-rst',
