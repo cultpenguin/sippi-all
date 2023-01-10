008597d965e9488fc649db9bc94f34120ab7b7ca
Git X-Modules <support@tmatesoft.com>
2023-01-10 10:13:39 +0000

    Protecting root commit

--- examples/conditional_uncertain_soft_data/figure_1_data.m
+++
@@ -1,8 +0,0 @@
-% figure_1_data
-TI = read_eas_matrix('ti.dat');
-m_ref = read_eas_matrix('m_ref.dat');
-
-figure(1);clf,
-subplot(1,2,1);imagesc(TI);title('Training Image');axis image;ax=axis;
-subplot(1,2,2);imagesc(m_ref);title('Reference model');;axis image;axis(ax);
-print('-dpng','figure_1_data')
